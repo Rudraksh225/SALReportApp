b631e2cdc53070ea4d2f287f1644693a1218a0c5
Rudraksh225 <rudrakshjani03@gmail.com>
2022-09-04 18:44:28 +0530

    adding a image storage and file filter function

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,34 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'}) 
+
+// Set a storage 
+const storage = multer.diskStorage({
+
+   // Set a destination where you want to store image
+   destination: function(req, file, cb){
+      cb(null, './uploads')
+   },
+
+   // Set a filename 
+   filename: function(req, file, cb){
+      cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname)
+   }
+});
+
+//set a file filter 
+const fileFilter = (req, file, cb) =>{
+
+   // Store a file id it is in jpeg, jpg or in png format, else don't store it
+   if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png'){
+      cb(null, true);
+   }
+   else{
+      cb(null, false);
+   }
+}
+
+const upload = multer({storage: storage});
 const Image = require('../models/Image')
 const fetchuser = require("../middleware/fetchuser");
 const { body, validationResult } = require("express-validator");
@@ -22,98 +49,28 @@ router.post("/fetchallpost", fetchuser, async (req, res) => {
 // ROUTE 2: add a new post using: POST "/api/post/addnote". Login required
 
 router.post("/addpost", fetchuser, upload.single('image'), async (req, res) => {
-   try {
-      const { img, area, description, locality, longtitude, latitude, phoneno, name } = req.body;
+   
+   try{
+      const { area, description, locality, longtitude, latitude, phoneno, name } = req.body;
+
       // If there are errors, return bad request and the errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
          return res.status(400).json({ errors: errors.array() });
       } 
-
-      // // Image Storage
-      // const Storage = multer.diskStorage({
-      //    destination: "uploads",
-      //    filename: (req, file, cb) => { 
-      //       cb(null, file.originalname);
-      //    },
-      // });
-      
-      // // Upload image at storage
-      // const upload = multer({
-      //    storage: Storage 
-      // }).single('testImage')
-
-      // // make a Image model with its id
-      // upload((err)=>{
-      //    if(err){
-      //       console.log(err)
-      //    }
-      //    else{
-      //       const newImage = new Image({
-      //          user: req.user.id,
-      //          image: {
-      //             data: req.file.filename,
-      //             contentType: "image/png"
-      //          },
-               
-      //       });
-      //       // save that image
-      //       const savedImage = newImage.save()
-      //    }
-      // })
-      
-
-      console.log(req.file);
-
-      // const findImage = await Image.findOne({user
-      //    :"req.user.id"})
-      // res.send(findImage.id)
       
       const newpost = new Post({
-         area, description, locality, longtitude, latitude, phoneno, name, user : req.user.id,
-         // img : findImage.id
+         phoneno, name, latitude, longtitude, area, description, locality, user: req.user.id
       });
 
 
       const savedPost = await newpost.save();
 
-      res.json(savedPost);
-
-   } catch (err) {
+      res.json(savedPost);   
+   }catch(err){
       console.error(err.message);
       res.status(500).send("Ineternal Server Error");
    }
 });
-
-// // Image Storage
-// const Storage = multer.diskStorage({
-//    destination: "uploads",
-//    filename: (req, file, cb) => { 
-//       cb(null, file.originalname);
-//    },
-// });
-
-// // Upload image at storage
-// const upload = multer({
-//    storage: Storage 
-// }).single('Img')
-
-// //ROUTE 3: upload an image using: POST "/api/post/upload"
-// router.post('/upload',(req,res)=>{
-//    upload(req,res,(err) => {
-//       if(err){
-//          console.log(err)
-//       }
-//       else{
-//          const newImage = new ImageModel({
-//             name: req.body.name,
-//             image:{
-//                data: req.file.filename,
-//                contentType: "image/jpg",
-//             }
-//          })
-//       }
-//    })
-// })
 
 module.exports = router;
