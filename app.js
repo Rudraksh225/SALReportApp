ecccd7c1038e72821eb2b84c119585018f6741b7
Rudraksh225 <rudrakshjani03@gmail.com>
2022-09-15 14:40:59 +0530

    a

--- app.js
+++ app.js
@@ -5,21 +5,14 @@ const connectToMongo = require('./db')
 const caches = require('apicache')
 const cache = caches.middleware
 
-// const url = 'mongodb://localhost/ReportApp' 
+
 connectToMongo()
 const app = express()
-// const api = "C1RfooeVScCf4TlPrM5mhI3zNmYcGmF59cDhvwVIjYYVkDhFJvTBJbMzE5uPIlMy"
-// mongoose.connect(url)
-// const con = mongoose.connection
-
-// con.on('open', function(){
-//     console.log('connected...')
-// })
 
 app.use(express.json({limit: '1000mb'}))
-app.use(cors('5 minutes'))
+app.use(cors())
 
-// app.use(cache())
+app.use(cache())
 
 /*Make a upload folder available publically for fetching images
   1. 1st part will be ignore '/upload' so you can access form browser with upload router also
@@ -28,41 +21,6 @@ app.use(cors('5 minutes'))
 */
 app.use('/uploads',express.static('uploads'))
 
-
-//Mongo DB API Key
-
-// var axios = require('axios');
-// var data = JSON.stringify({
-//     "collection": "users",
-//     "database": "test",
-//     "dataSource": "Cluster0",
-//     "projection": {
-//         "_id": 1
-//     }
-// });
-            
-// var config = {
-//     method: 'post',
-//     url: 'https://data.mongodb-api.com/app/data-jaabs/endpoint/data/v1/action/findOne',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Access-Control-Request-Headers': '*',
-//       'api-key': 'ADrCFDhxwapdkRbNRbGAMn8S5qFNVwoVdmu6tRvnlmLIo0nkNE0SC6XfumRuAoTf',
-//     },
-//     data: data
-// };
-            
-// axios(config)
-//     .then(function (response) {
-//         console.log(JSON.stringify(response.data));
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-
-
-// const url = 'https://data.mongodb-api.com/app/data-jaabs/endpoint/data/v1'
-// api = "ADrCFDhxwapdkRbNRbGAMn8S5qFNVwoVdmu6tRvnlmLIo0nkNE0SC6XfumRuAoTf"
 //Available routes
 // app.use(url + api + '/auth', require('./routes/auth'))
 app.use('/api/auth', require('./routes/auth'));
