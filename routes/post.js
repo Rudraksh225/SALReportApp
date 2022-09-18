994d47e7cac6dfe2e5166f04855aeb6193b32bb3
Rudraksh225 <rudrakshjani03@gmail.com>
2022-09-18 14:45:31 +0530

    a

--- routes/post.js
+++ routes/post.js
@@ -149,8 +149,7 @@ router.post("/addpost", async (req, res) => {
          phoneno, name, latitude, longtitude, area, description, locality, user, image 
       });
       }catch(err){
-         
-         res.msg(err)
+
          console.log(aaaaaaaaaaaaaaaaaaaaa)
          console.log(err)
          console.log(aaaaaaaaaaaaaaaaaaaaa)
