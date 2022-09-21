06b58244974f97307a4148f88e7af7b8da358a56
Rudraksh225 <rudrakshjani03@gmail.com>
2022-09-21 17:38:51 +0530

    a

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const Admin = require('../models/Admin');
 const fetchuser = require("../middleware/fetchuser");
 const { body, validationResult } = require("express-validator");
 
