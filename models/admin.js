d4b9a8f9c2bacd175ada3996377ee1d1458e751f
Rudraksh225 <rudrakshjani03@gmail.com>
2022-09-21 18:00:43 +0530

    a

--- models/admin.js
+++ models/admin.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-
     email:{
         type: String,
         required: true,
