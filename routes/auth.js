ff734c7cc9b5ada79a96d5f604e59bd77fe30f82
Rudraksh225 <rudrakshjani03@gmail.com>
2022-09-21 13:28:23 +0530

    a

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const Admin = require('../models/Admin');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const fetchuser = require('../middleware/fetchuser');
-const Admin = require('../models/Admin')
 // import Admin from '../models/Admin'
 
 // try{
